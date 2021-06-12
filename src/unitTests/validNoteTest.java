33e783135af627cbbeff98a7c9caa3ff5a324132
mateasv <math059j@easv365.dk>
2021-06-12 03:16:11 +0200

    Improved Unit Tests Readability.

--- src/unitTests/validNoteTest.java
+++ src/unitTests/validNoteTest.java
@@ -6,27 +6,33 @@ public class validNoteTest {
     @org.junit.jupiter.api.Test
     void noteText() {
         // The text is within the character limit.
-        Assertions.assertTrue(Verifier.verifyNoteData("This is a short and valid message.", "2", "1"));
+        boolean verified = Verifier.verifyNoteData("This is a short and valid message.", "2", "1");
+        Assertions.assertTrue(verified);
 
         // The text is above the character limit.
-        Assertions.assertFalse(Verifier.verifyNoteData("This text goes over the character limit! Lorem ipsum dolor sit amet, consectetuer dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate.", "2", "1"));
+        verified = Verifier.verifyNoteData("This text goes over the character limit! Lorem ipsum dolor sit amet, consectetuer dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate.", "2", "1");
+        Assertions.assertFalse(verified);
     }
 
     @org.junit.jupiter.api.Test
     void noteOrder() {
         // The order argument is valid
-        Assertions.assertTrue(Verifier.verifyNoteData("Note text.", "20", "1"));
+        boolean verified =Verifier.verifyNoteData("Note text.", "20", "1");
+        Assertions.assertTrue(verified);
 
         // The order argument is invalid
-        Assertions.assertFalse(Verifier.verifyNoteData("Note text.", "twenty", "1"));
+        verified = Verifier.verifyNoteData("Note text.", "twenty", "1");
+        Assertions.assertFalse(verified);
     }
 
     @org.junit.jupiter.api.Test
     void noteRow() {
         // The row argument is valid
-        Assertions.assertTrue(Verifier.verifyNoteData("Note text.", "2", "20"));
+        boolean verified = Verifier.verifyNoteData("Note text.", "2", "20");
+        Assertions.assertTrue(verified);
 
         // The row argument is invalid
-        Assertions.assertFalse(Verifier.verifyNoteData("Note text.", "2", "twenty"));
+        verified =Verifier.verifyNoteData("Note text.", "2", "twenty");
+        Assertions.assertFalse(verified);
     }
 }
